96ea50bb093ebd8e538fca64a115f679c8585fd7
njhan2 <njhan2@illinois.edu>
2025-02-17 14:56:50 -0600

    added files

---
+++ backdropper/backdropper.py
@@ -0,0 +1,9 @@
+import bpy
+
+# premise:
+    #import image file
+    #contextual depth mapping
+    #insert plane w/ relevant extrusions
+    #generate displacement/bump map
+    #apply and unwrap UV
+
---
+++ backdropper/generate_depth_map.py
@@ -0,0 +1,12 @@
+#generate depth layer:
+    #find low contrast areas
+    #generate and scale squares until collision
+    # proportion of square * light level = depth
+
+#disjoint set?
+
+#for each pixel in image
+#merge with neighbor/set with lowest contrast
+
+from PIL import Image
+
