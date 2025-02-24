bc92e825da5ba0b7538c0cd1dd19751891bc5bc4
njhan2 <njhan2@illinois.edu>
2025-02-24 14:49:55 -0600

    makefile added

--- backdropper/color_reduction.py
+++
@@ -1,8 +0,0 @@
-#  sample unique colors -> hash table
-# assign weights to each point proportional to frequency -> normalize
-# precalculate pairwise distance -> skip next if a curr center is far away
-# WSM algorithm:
-
-# initialization schemes select centers -> look into this
-
-# perhaps use cython?
